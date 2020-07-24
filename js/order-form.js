a08d57f187e676f623684237dfba3ea115b04b9b
BaloghGabor <bgdozsa@gmail.com>
2020-07-24 11:54:52 +0200

    05-23 verzió

--- js/order-form.js
+++ js/order-form.js
@@ -1,9 +1,25 @@
 function sendOrderData() {
-    const message = document.querySelector("#message")
+    const name = document.querySelector("#name").value.trim();
+    const email = document.querySelector("#email").value.trim();
+    const address = document.querySelector("#address").value.trim();
+    const comment = document.querySelector("#comment").value.trim();
     const extra = parseInt(document.querySelector("[name=extra]:checked").value)
-    //console.log(extra)
     const sauce = parseInt(document.querySelector("#sauce").value)
     const quantity = parseInt(document.querySelector("#quantity").value)
-    const price = (1200 + extra + sauce) * quantity
-    message.innerText = price;
+    
+    if (!name) {
+        alert("A név megadása kötelező!");
+    } else if (!email || !(email.indexOf("@") > 0) || !(email.indexOf(".") > 0)) {
+        alert("Érvénytelen e-mail cím!");
+    } else if (address.length < 10) {
+        alert("Kérjük pontos címet adj meg!");
+    } else if ((comment.indexOf("<") >= 0) && (comment.indexOf(">") > 0)) {
+        alert("A megjegyzésben nem megengedett a HTML használata!");
+    } else if (!quantity || quantity < 1 || quantity > 10) {
+        alert("Csak 1 és 10 között rendelhetsz!");
+    } else {        
+        const message = document.querySelector("#message");
+        const price = (1200 + extra + sauce) * quantity;
+        message.innerText = price;
+    }
 }
