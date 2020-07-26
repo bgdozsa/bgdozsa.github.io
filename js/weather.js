136dbed14be3fda3430c418aa3e1e02dd2b80a53
BaloghGabor <bgdozsa@gmail.com>
2020-07-26 14:02:30 +0200

    08-08 verzió

--- js/weather.js
+++ js/weather.js
@@ -1,24 +1,35 @@
-let temperatures = [-11.2, 14.4, 13.0, 17.3, 21.7, 18.2, 28]
-let temperatureUpperLimits = [0, 15, 20, 25, 99]
-let offers = [
-    "Ma forró csokit is árusítunk.",
-    "Melegedj át egy teával nálunk!",
-    "Ma finom sütivel várunk.",
-    "Ma fagyit is kínálunk.",
-    "Hűtsd le magad egy jéghideg limonádéval!"]
+function getWeatherData() {
+    let adatok =
+    {
+        "weathers": [
+            {"dayNumber": 0, "temperature": -11.2},
+            {"dayNumber": 1, "temperature": 14.4},
+            {"dayNumber": 2, "temperature": 13.0},
+            {"dayNumber": 3, "temperature": 17.3},
+            {"dayNumber": 4, "temperature": 21.7},
+            {"dayNumber": 5, "temperature": 18.2},
+            {"dayNumber": 6, "temperature": 28.0}
+        ],
+        "offers": [
+            {"upperLimit": 0, "offerMessage": "Ma forró csokit is árusítunk."},
+            {"upperLimit": 15, "offerMessage": "Melegedj át egy teával nálunk!"},
+            {"upperLimit": 20, "offerMessage": "Ma finom sütivel várunk."},
+            {"upperLimit": 25, "offerMessage": "Ma fagyit is kínálunk."},
+            {"upperLimit": 99, "offerMessage": "Hűtsd le magad egy jéghideg limonádéval!"}
+        ]
+    }
+    return adatok
+}
 
+let data = getWeatherData()
 
 function weatherWidget() {
     const day = document.querySelector("#day").value
     const temperatureDiv = document.querySelector("#temperature")
-    temperatureDiv.innerHTML = temperatures[day] + " &deg;C"
-
-    for (let i = 0; i < temperatureUpperLimits.length; i++) {
-        if (temperatures[day] <= temperatureUpperLimits[i]) {
-            temperatureDiv.innerHTML += "<br>" + offers[i]
-            break
-        }
-    }
+    const temperature = findWeather(day).temperature
+    const offerMessage = findOffer(temperature).offerMessage
+    temperatureDiv.innerHTML = temperature + " &deg;C"
+    temperatureDiv.innerHTML += "<br><span class='offer'>" + offerMessage + "</span>"
 
     const minDiv = document.querySelector("#min")
     minDiv.innerHTML = "Minimum: " + minTemperature() + " &deg;C"
@@ -28,12 +39,28 @@ function weatherWidget() {
     avgDiv.innerHTML = "Átlag: " + avgTemperature().toFixed(2) + " &deg;C"
 }
 
+function findWeather(day) {
+    for (let weather of data.weathers) {
+        if (weather.dayNumber == day) {
+            return weather
+        } 
+    }
+}
+
+function findOffer(temp) {
+    for (let offer of data.offers) {
+        if (temp <= offer.upperLimit) {
+            return offer;
+        }
+    }
+}
+
 
 function minTemperature() {
-    let min = temperatures.length != 0 ? temperatures[0] : 0
-    for (let i = 0; i < temperatures.length; i++) {
-        if (min > temperatures[i]) {
-            min = temperatures[i]
+    let min = data.weathers.length != 0 ? data.weathers[0].temperature : 0
+    for (let i = 0; i < data.weathers.length; i++) {
+        if (min > data.weathers[i].temperature) {
+            min = data.weathers[i].temperature
         }        
     }
     return min
@@ -41,10 +68,10 @@ function minTemperature() {
 
 
 function maxTemperature() {
-    let max = temperatures.length != 0 ? temperatures[0] : 0
-    for (let i = 0; i < temperatures.length; i++) {
-        if (max < temperatures[i]) {
-            max = temperatures[i]
+    let max = data.weathers.length != 0 ? data.weathers[0].temperature : 0
+    for (let i = 0; i < data.weathers.length; i++) {
+        if (max < data.weathers[i].temperature) {
+            max = data.weathers[i].temperature
         }        
     }
     return max
@@ -53,8 +80,8 @@ function maxTemperature() {
 
 function avgTemperature() {
     let avg = 0
-    for (let i = 0; i < temperatures.length; i++) {
-        avg += temperatures[i]
+    for (let i = 0; i < data.weathers.length; i++) {
+        avg += data.weathers[i].temperature
     }
-    return temperatures.length != 0 ? avg / temperatures.length : 0
+    return data.weathers.length != 0 ? avg / data.weathers.length : 0
 }
